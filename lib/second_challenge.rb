8cbc5f885906178890204e60430c963d0a1239b0
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-10-21 04:32:42 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,7 +3,7 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       :patriarch => {},
-      :matirarch => {},
+      :matriarch => {},
       :hero => {},
       :hero_friends => []
     }, 
